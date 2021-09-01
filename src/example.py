f82c737244a3be61dbcad23ceb81ba61a1d94191
Negated-py <kevin.negated@gmail.com>
2021-09-01 17:01:36 +0700

    Added Example

---
+++ src/example.py
@@ -0,0 +1,8 @@
+from avlc.core import AudioPlayer
+
+a = AudioPlayer()
+a.add_media("AudioExample/Eliminate - This Song Is Copyright Free.mp3")
+a.add_media("AudioExample/TheFatRat & Slaydit - Solitude.mp3")
+a.play()
+
+input()
